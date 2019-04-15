19fbe9a5ed029397aeec58190c41b7a599aed6a2
Timothy Grey <tgrey@assure.co>
2019-04-15 11:12:53 -0600

    intial app setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-delaunay-canvas';
+  @ViewChild('canvasEl') canvasEl: ElementRef;
+
+  /** Canvas 2d context */
+  private ctx: CanvasRenderingContext2D;
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.ctx = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.canvasEl.nativeElement.width = 500
+    this.canvasEl.nativeElement.height = 500
+
+    this.draw();
+  }
+
+  private draw() {
+    this.ctx.fillStyle = 'green';
+    
+    const x = this.canvasEl.nativeElement.width / 2;
+    const y = this.canvasEl.nativeElement.height / 2;
+
+    this.ctx.fillRect(x, y, 50, 50);
+  }
+
+
 }
